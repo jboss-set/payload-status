4e2b8be9a333b36aec64f21052f6e023c016577a
Michal Petrov <mpetrov@redhat.com>
2020-09-11 17:04:19 +0200

    formatting

--- src/IssueTables.js
+++ src/IssueTables.js
@@ -7,64 +7,64 @@ import { Title } from '@patternfly/react-core';
 import { Spinner } from '@patternfly/react-core';
 
 const standaloneColumns = [
-    { title: "Number", transforms: [sortable] },
-    { title: "Priority", transforms: [sortable] },
-    { title: "Status", transforms: [sortable] },
-    { title: "Name", columnTransforms: [classNames('issue-name')] },
-    { title: "Type", columnTransforms: [classNames('issue-type')] },
-    { title: "Acks" },
-    { title: "PR Status", transforms: [sortable] },
-    { title: "Upstream" }
+  { title: "Number", transforms: [sortable] },
+  { title: "Priority", transforms: [sortable] },
+  { title: "Status", transforms: [sortable] },
+  { title: "Name", columnTransforms: [classNames('issue-name')] },
+  { title: "Type", columnTransforms: [classNames('issue-type')] },
+  { title: "Acks" },
+  { title: "PR Status", transforms: [sortable] },
+  { title: "Upstream" }
 ];
 
 const upgradeColumns = [
-    { title: "Number" },
-    { title: "Priority" },
-    { title: "Status" },
-    { title: "Name", columnTransforms: [classNames('issue-name')] },
-    { title: "Type", columnTransforms: [classNames('issue-type')] },
-    { title: "Acks" },
-    { title: "PR Status" },
-    { title: "Upstream" }
+  { title: "Number" },
+  { title: "Priority" },
+  { title: "Status" },
+  { title: "Name", columnTransforms: [classNames('issue-name')] },
+  { title: "Type", columnTransforms: [classNames('issue-type')] },
+  { title: "Acks" },
+  { title: "PR Status" },
+  { title: "Upstream" }
 ];
 
 const IssueTables = ({data,setRows}) => {
-    if (data.loading) {
-        return <Spinner />
-    }
-    if (data.error != null || data.upgradesTotal === 0) {
-        return <MessageBar error={data.error} />
-    }
+  if (data.loading) {
+    return <Spinner />
+  }
+  if (data.error != null || data.upgradesTotal === 0) {
+    return <MessageBar error={data.error} />
+  }
 
-    const updateStandaloneRows = (standalone) => {
-        setRows(prevState => ({
-            ...prevState,
-            standalone: {
-              rows: standalone.rows,
-              sortBy: standalone.sortBy
-            }
-        }))
-    };
+  const updateStandaloneRows = (standalone) => {
+    setRows(prevState => ({
+        ...prevState,
+        standalone: {
+          rows: standalone.rows,
+          sortBy: standalone.sortBy
+        }
+    }))
+  };
 
-    return (
-        <div>
-            <Title headingLevel="h1" size="xl">{`${data.standalone.rows.length + data.upgrades.rows.length} issues in payload`}</Title>
-            <IssueTable
-                caption={`${data.standalone.rows.length} Standalone issues`}
-                className="standalone"
-                columns={standaloneColumns}
-                rows={data.standalone.rows}
-                sortBy={data.standalone.sortBy}
-                updateRows={updateStandaloneRows}
-            />
-            <IssueTable
-                caption={`${data.upgradesTotal} Component upgrades, ${data.upgrades.rows.length} issues total`}
-                className="upgrades"
-                columns={upgradeColumns}
-                rows={data.upgrades.rows}
-            />
-        </div>
-    )
+  return (
+    <div>
+      <Title headingLevel="h1" size="xl">{`${data.standalone.rows.length + data.upgrades.rows.length} issues in payload`}</Title>
+      <IssueTable
+        caption={`${data.standalone.rows.length} Standalone issues`}
+        className="standalone"
+        columns={standaloneColumns}
+        rows={data.standalone.rows}
+        sortBy={data.standalone.sortBy}
+        updateRows={updateStandaloneRows}
+      />
+      <IssueTable
+        caption={`${data.upgradesTotal} Component upgrades, ${data.upgrades.rows.length} issues total`}
+        className="upgrades"
+        columns={upgradeColumns}
+        rows={data.upgrades.rows}
+      />
+    </div>
+  )
 }
 
 export default IssueTables
--- src/PayloadPicker.js
+++ src/PayloadPicker.js
@@ -4,42 +4,43 @@ import { Toolbar , ToolbarItem, ToolbarContent } from '@patternfly/react-core';
 import { Select, SelectOption } from '@patternfly/react-core';
 
 const PayloadPicker = ({onSelect, data}) => {
-    const [isOpen, setOpen] = useState(false);
-    const [selected, setSelected] = useState(null);
+  const [isOpen, setOpen] = useState(false);
+  const [selected, setSelected] = useState(null);
 
-    if (data.error != null || data.list == null || Object.keys(data.list).length === 0) {
-        return <MessageBar error={data.error || "No payloads loaded"} />
-    }
+  if (data.error != null || data.list == null || Object.keys(data.list).length === 0) {
+    return <MessageBar error={data.error || "No payloads loaded"} />
+  }
 
-    const select = (e, value) => {
-        onSelect(value);
-        setSelected(value);
-        setOpen(!isOpen);
-    }
+  const select = (e, value) => {
+    onSelect(value);
+    setSelected(value);
+    setOpen(!isOpen);
+  }
 
-    const flatList = flattenList(data.list);
+  const flatList = flattenList(data.list);
 
-    const items = (
-      <>
-        <ToolbarItem variant="label">Payload Overview</ToolbarItem>
-        <ToolbarItem>
-          <Select onSelect={select} onToggle={setOpen} isOpen={isOpen} selections={selected}>
-          {flatList.map((item, index) => (
-            <SelectOption key={index} value={item} />
-          ))}
-          </Select>
-        </ToolbarItem>
-      </>);
+  const items = (
+    <>
+      <ToolbarItem variant="label">Payload Overview</ToolbarItem>
+      <ToolbarItem>
+        <Select onSelect={select} onToggle={setOpen} isOpen={isOpen} selections={selected}>
+        {flatList.map((item, index) => (
+          <SelectOption key={index} value={item} />
+        ))}
+        </Select>
+      </ToolbarItem>
+    </>
+  );
 
-    return <Toolbar id="toolbar"><ToolbarContent>{items}</ToolbarContent></Toolbar>;
+  return <Toolbar id="toolbar"><ToolbarContent>{items}</ToolbarContent></Toolbar>;
 }
 
 const flattenList = (list) => {
-    let flatList = [];
-    for (let key in list) {
-        list[key].forEach(item => flatList.push(`${key}/${item}`));
-    }
-    return flatList;
+  let flatList = [];
+  for (let key in list) {
+    list[key].forEach(item => flatList.push(`${key}/${item}`));
+  }
+  return flatList;
 }
 
 export default PayloadPicker;

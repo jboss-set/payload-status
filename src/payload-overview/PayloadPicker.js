c8907f86724ed1135a0d396276704e471b4c965c
Michal Petrov <mpetrov@redhat.com>
2020-11-04 15:46:37 +0100

    add toggle for latest payloads only

--- src/payload-overview/PayloadPicker.js
+++ src/payload-overview/PayloadPicker.js
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
 import { Toolbar , ToolbarItem, ToolbarContent } from '@patternfly/react-core';
 import { Select, SelectOption } from '@patternfly/react-core';
+import { Checkbox } from '@patternfly/react-core';
 import { defaultOption } from '../common/Util';
 
 const PayloadPicker = ({onSelect, data}) => {
   const [isOpen, setOpen] = useState(false);
   const [selected, setSelected] = useState(null);
+  const [isLatestOnly, setLatestOnly] = useState(true);
 
   const select = (e, val, isPlaceholder) => {
     let value = isPlaceholder ? "" : val;
@@ -14,7 +16,10 @@ const PayloadPicker = ({onSelect, data}) => {
     setOpen(!isOpen);
   }
 
-  const flatList = flattenList(data.list);
+  let flatList = flattenList(data.list).sort().reverse();
+  if (isLatestOnly) {
+    flatList = flatList.filter(item => item.includes("7.3")).slice(0,2).concat(flatList.filter(item => item.includes("7.2")).slice(0,2));
+  }
 
   const items = (
     <>
@@ -26,6 +31,9 @@ const PayloadPicker = ({onSelect, data}) => {
           <SelectOption key={index} value={item} />
         ))}
         </Select>
+      </ToolbarItem>
+      <ToolbarItem>
+        <Checkbox label="Latest only" aria-label="latest only" id="latest-check" isChecked={isLatestOnly} onChange={setLatestOnly} />
       </ToolbarItem>
     </>
   );
